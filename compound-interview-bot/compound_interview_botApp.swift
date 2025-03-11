f61f8fc3af122d189ac232079f1dd5e88d744c1e
ranjith <ranjith.perkam14@gmail.com>
2025-03-11 12:17:52 -0500

    initial commit

---
+++ compound-interview-bot/compound_interview_botApp.swift
@@ -0,0 +1,17 @@
+//
+//  compound_interview_botApp.swift
+//  compound-interview-bot
+//
+//  Created by ranjith kumar reddy b perkampally on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct compound_interview_botApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
